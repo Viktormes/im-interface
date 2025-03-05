bf76d6ac532be59ce8e6f70614e20213ef210581
Viktor Hägglund <vikha11@vgregion.se>
2025-03-05 11:28:34 +0100

    fixes

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE IF NOT EXISTS iim_interface;
 
--- Check if the user exists before creating it
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON iim_interface.* TO 'user'@'%';
 FLUSH PRIVILEGES;
