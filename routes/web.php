692d2a2ed9978914c18c868737d9865175fbb8d2
Viktor Hägglund <vikha11@vgregion.se>
2025-03-12 10:55:31 +0100

    aaaa

--- routes/web.php
+++ routes/web.php
@@ -10,25 +10,30 @@ use App\Http\Controllers\TreatmentController;
 use App\Http\Middleware\RequireSelectedPatientMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/patients');
+Route::redirect('/', '/im-interface/patients');
 
 require __DIR__.'/auth.php';
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Add the im-interface prefix group
+Route::prefix('im-interface')->group(function () {
+    // Unauthenticated routes
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/patients', PatientsListController::class)->name('patients');
-    Route::get('/patients/select/{patient}', [SelectedPatientController::class, 'store']);
-    Route::delete('/patients/select', [SelectedPatientController::class, 'destroy']);
+    // Authenticated routes
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('/patients', PatientsListController::class)->name('patients');
+        Route::get('/patients/select/{patient}', [SelectedPatientController::class, 'store']);
+        Route::delete('/patients/select', [SelectedPatientController::class, 'destroy']);
 
-    Route::middleware([RequireSelectedPatientMiddleware::class])->group(function () {
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-        Route::get('/overview', [OverviewController::class, 'index'])->name('overview');
-        Route::get('/measurements', [MeasurementsController::class, 'index'])->name('measurements');
-        Route::get('/treatment', [TreatmentController::class, 'index'])->name('treatment');
+        Route::middleware([RequireSelectedPatientMiddleware::class])->group(function () {
+            Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+            Route::get('/overview', [OverviewController::class, 'index'])->name('overview');
+            Route::get('/measurements', [MeasurementsController::class, 'index'])->name('measurements');
+            Route::get('/treatment', [TreatmentController::class, 'index'])->name('treatment');
+        });
     });
 });
